198413411f2c2d5492d47895e91d021a62911f57
Bronson Brown-deVost <bronsonbdevost@aim.com>
2019-01-08 12:56:44 +0100

    Updated tei processor.

--- data-processing/build-tei.py
+++ data-processing/build-tei.py
@@ -1,30 +1,51 @@
 # This is still incomplete.
 
-import codecs
-import re
-
-f = codecs.open('Cod_Sach_339.txt', encoding='utf-8')
-l = 0
-p = 0
-text = "<pb>"
-for line in f:
+import codecs, re, sys, getopt, os
+def main(argv):
+    inputfile = "Cod_Sach_339.txt"
+    outputfolder = "./"
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofolder="])
+    except getopt.GetoptError:
+        print 'test.py -i <inputfile> -o <outputfolder>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print 'test.py -i <inputfolder> -o <outputfolder>'
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            inputfile = arg
+        elif opt in ("-o", "--ofolder"):
+            outputfolder = arg
+    print 'Converting', inputfile, "to tei xml format."
+    f = codecs.open(inputfile, encoding='utf-8')
+    l = 0
+    p = 0
     w = 0
-    l += 1
-    if l == 1:
-        text += "<line n=\"" + str(l) + "\">"
-    else:
-        text += "</line><line n=\"" + str(l) + "\">"
-    for word in line.split():
-        w += 1
-        page = re.search(r'\d{1,4}', word)
-        if page:
-            if p == 0:
+    currentLine = 0
+    text = "<body>"
+    for line in f:
+        currentLine += 1
+        for word in line.split():
+            w += 1
+            page = re.search(r'\d{1,4}', word)
+            if page:
                 p += 1
-                text = text[:3] + " xml:id=\"" + page.group() + "\" n=\"" + str(p) + "\">"
+                text += "<pb n=\"" + str(p) + "\"/>"
+                currentLine = 0
+                l = currentLine + 1
+                text += "<lb n=\"" + str(1) + "\"/>"
             else:
-                text += "</pb><pb xml:id=\"" + page.group() + "\" n=\"" + str(p) + "\">"
-            l = 0
-            p += 1
-        else:
-            text += "<w n=\"" + str(w) + "\">" + word + "</w>"
-print(text + "</line></pb>")
+                if currentLine != l:
+                    l = currentLine
+                    text += "<lb n=\"" + str(currentLine) + "\"/>"
+                text += "<w n=\"" + str(w) + "\">" + word + "</w>"
+    text += "</body>"
+    outfile = outputfolder + os.path.splitext(os.path.basename(inputfile))[0] + ".tei"
+    print "The file has been output to:", outfile
+    file = codecs.open(outfile, "w", "utf-8")
+    file.write(text)
+    file.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
